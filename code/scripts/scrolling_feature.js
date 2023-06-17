ba36fe7061b7659439125c6f36bcac7b8924aab0
AbuShl123 <shoalievabubakr@gmail.com>
2023-06-17 14:42:50 +0600

    Updated transitions

--- code/scripts/scrolling_feature.js
+++ code/scripts/scrolling_feature.js
@@ -1,7 +1,37 @@
 let moduleNumber = 1;
+let moduleName = "module" + moduleNumber;
 
 function animationOnExit() {
-  document.getElementById("module" + moduleNumber).className = 'container animationOnExit';
+  document.getElementById(moduleName).className = 'container animationOnExit';
+  const exitingElement = document.querySelector('#' + moduleName + '.animationOnExit');
+
+  exitingElement.addEventListener('animationend', () => {
+    exitingElement.className += ' ghost';
+
+    updateModuleVars();
+
+    document.getElementById(moduleName).className = 'container animationOnStart';
+    const openingElement = document.querySelector('#' + moduleName + '.animationOnExit');
+    openingElement.addEventListener('animationend', () => {
+      openingElement.className += ' cancel_ghost';
+    });
+  });
+}
+
+function hideElementAfterAnimationCompleted(exitingElement) {
+  exitingElement.addEventListener('animationend', () => {
+    exitingElement.className += " ghost";
+  });
+}
+
+function openElementAfterAnimationCompleted(openingElement) {
+  openingElement.addEventListener('animationend', () => {
+    openingElement.className += " cancel_ghost";
+  });
+}
+
+function updateModuleVars() {
   moduleNumber += 1;
-  document.getElementById("module" + moduleNumber).className += ' animationOnStart';
+  moduleName = "module" + moduleNumber;
+  console.log(moduleName);
 }
