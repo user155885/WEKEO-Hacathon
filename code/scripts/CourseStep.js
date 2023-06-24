7f49ba564882486c1f8d70e4bda80ccd891b4cc4
AbuShl123 <shoalievabubakr@gmail.com>
2023-06-24 20:45:22 +0600

    Website - finished

--- code/scripts/CourseStep.js
+++ code/scripts/CourseStep.js
@@ -42,3 +42,38 @@ function updateCourse(stepNumber, courseStep) {
     currentSlide = null; // very important line!! Delete - and you will see the amazing motions in the website
   });
 }
+
+window.addEventListener('load', function() {
+  var parent = document.querySelector('div.course_video');
+  var child = document.querySelector('div.course_video > iframe');
+  
+  function setChildHeight() {
+    var parentWidth = parent.offsetWidth;
+    var childHeight = (parentWidth * 0.58) + 'px';
+    child.style.height = childHeight;
+  }
+  
+  setChildHeight(); // Set the initial height
+  
+  // Update the child's height when the window is resized
+  window.addEventListener('resize', setChildHeight);
+});
+
+function checkAnswer(answer, QNumber) {
+  if (!answer.className.includes("button-hover")) return;
+
+  if (answer.className.includes("button_wrong")) {
+    answer.className += " selected_wrong";
+  } else {
+    answer.className += " selected_correct";
+    const otherOptions = document.querySelectorAll("*[for='" + QNumber + "']");
+    otherOptions.forEach((option) => {
+      option.className = option.className
+                        .replace("button", "button-noniteractable")
+                        .replace("button-hover", "");
+      option = null; 
+    });
+  }
+  answer.className = answer.className.replace("button-hover", "");
+  answer = null;
+}
